71bbfe5cb3cb2084ff28e78ec74d5792bf365403
Lara <dundar.lara@gmail.com>
2024-04-23 12:51:20 +0200

    Corrected and commented on FileOutputStrategy about Exception type.

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -37,7 +37,7 @@ public class FileOutputStrategy implements OutputStrategy {
         try (PrintWriter out = new PrintWriter(
                 Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
-        } catch (Exception e) {
+        } catch (IOException e) { // Changed the generic Exception to more specific IOException because it covers file-related errors.
             System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
         }
     }
